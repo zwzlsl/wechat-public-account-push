9e8710f9984c95dc86f4bb8ac7481ce7500a9650
zwzlsl <119159442+zwzlsl@users.noreply.github.com>
2023-10-07 17:33:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oGsy-6XhuPfPuS9KEKV1eUacCRno',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'yI3kXLZtT78iL_uVOoDD3OMoocFvWXvmZIvyumIWeg0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'yI3kXLZtT78iL_uVOoDD3OMoocFvWXvmZIvyumIWeg0',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oGsy-6XhuPfPuS9KEKV1eUacCRno',
     }
   ],
 
